be3e1f35ca5021ee16fc87b15ba5d14db6d854ff
hoangphuc91 <hoangphuc91dtt@gmail.com>
2023-03-09 00:25:26 +0700

    feature: create profile cv

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const informationController = require("../controller/informationController");
+const educationController = require("../controller/educationController");
+const skillController = require("../controller/skillController");
+const experienceController = require("../controller/experienceController");
+const main = require("../controller/main");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+router.get("/", main.index);
+// router.get("/", educationController.index);
+// router.get("/", experienceController.index);
+// router.get("/", skillController.index);
 module.exports = router;
