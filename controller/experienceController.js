791f53019b1e557929f86d7449910167789e8258
TrongQuang12 <quang866n@gmail.com>
2023-03-11 13:09:05 +0700

    quang

--- controller/experienceController.js
+++ controller/experienceController.js
@@ -3,7 +3,6 @@ const Experience = require("../models/experienceModel");
 async function index(req, res, next) {
     const experiences = await Experience.find({});
 
-    console.log(experiences);
 
     res.render("index", {
         experiences,
